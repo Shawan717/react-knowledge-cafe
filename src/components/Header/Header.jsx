2301b4b859dfd584892baefa18eaf73de6da9a02
Shawan717 <shawantalukder4@gmail.com>
2023-12-16 01:20:49 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-3xl font-bold text-center text-orange-400'>Vite + React</h1>
+        </div>
+    );
+};
+
+export default Header;
